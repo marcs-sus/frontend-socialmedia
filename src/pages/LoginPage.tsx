bddbee1eb7891f75917e1b4ac32c7110cafcd290
marcs-sus <marcosviniciuscunha@proton.me>
2026-06-21 13:04:17 -0300

    Create Login and Register pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,58 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { loginUser } from "../api/authService";
+
 export default function LoginPage() {
-  return <h1>Login</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+
+  const { login } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await loginUser({
+        email,
+        password,
+      });
+
+      login(response);
+
+      navigate("/communities");
+    } catch {
+      setError("Invalid credentials.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,3 +1,67 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { registerUser } from "../api/authService";
+
 export default function RegisterPage() {
-  return <h1>Register</h1>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+
+  const { login } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await registerUser({
+        username,
+        email,
+        password,
+      });
+
+      login(response);
+
+      navigate("/communities");
+    } catch {
+      setError("Invalid credentials.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
