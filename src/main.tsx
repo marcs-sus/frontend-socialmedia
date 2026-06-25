58666ce558229cde8229039b8495f42509b1a734
marcs-sus <marcosviniciuscunha@proton.me>
2026-06-24 22:12:40 -0300

    Add simple styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { AuthProvider } from "./context/AuthContext";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./styles/global.css";
+import "./styles/navbar.css";
+import "./styles/forms.css";
+import "./styles/cards.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
